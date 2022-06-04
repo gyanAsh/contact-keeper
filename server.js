4dbe84c10e78ed81144f6068936a53ed4a9bc305
gyanAsh <gyanash36@protonmail.com>
2022-06-04 19:31:44 +0530

    Connection to database establised in db.js file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+//Connect to database
+connectDB();
 
 //Defining Routes
 app.use('/api/v1/users', require('./routes/users'));
